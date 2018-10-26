ae8caeb8c17e4d82b00f288d7306b1263be35f53
meng <mengyueyao>
2018-10-26 18:42:54 +0800

    修改linearlayoutShape和editextShape不会提示问题

--- shape/src/main/java/shape/meng/com/shape/LinerLayoutShape.java
+++ shape/src/main/java/shape/meng/com/shape/LinerLayoutShape.java
@@ -5,6 +5,7 @@ import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
 
@@ -26,9 +27,15 @@ public class LinerLayoutShape extends LinearLayout {
         int roundRadius = (int)typedArray.getDimension(R.styleable.LinerLayoutShape_roundRadius,0f); //  圆角半径
         int strokeColor = typedArray.getColor(R.styleable.LinerLayoutShape_strokeColor, Color.parseColor("#00000000"));//边框颜色
         int solidColor = typedArray.getColor(R.styleable.LinerLayoutShape_solidColor, Color.parseColor("#00000000"));//内部填充颜色
-        int shape = typedArray.getInt(R.styleable.LinerLayoutShape_shape,0);  //0：方形  1：圆形   默认是0    如果需要设置成圆形空间的宽高必须相等  否则为椭圆
+        String shape = typedArray.getString(R.styleable.TextShape_shape);  //0：方形  1：圆形   默认是0    如果需要设置成圆形空间的宽高必须相等  否则为椭圆
         GradientDrawable gd = new GradientDrawable();//创建drawable
-        gd.setShape(shape);
+        if(!TextUtils.isEmpty(shape)&&shape.equals("oval")){
+            gd.setShape(GradientDrawable.OVAL);
+
+        }else{
+
+            gd.setShape(GradientDrawable.RECTANGLE);
+        }
         gd.setColor(solidColor);
         gd.setCornerRadius(roundRadius);
         gd.setStroke(strokeWidth, strokeColor);
--- shape/src/main/java/shape/meng/com/shape/TextShape.java
+++ shape/src/main/java/shape/meng/com/shape/TextShape.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.AppCompatTextView;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 
 
@@ -26,9 +27,16 @@ public class TextShape extends AppCompatTextView {
         int roundRadius = (int)typedArray.getDimension(R.styleable.TextShape_roundRadius,0f); //  圆角半径
         int strokeColor = typedArray.getColor(R.styleable.TextShape_strokeColor, Color.parseColor("#00000000"));//边框颜色
         int solidColor = typedArray.getColor(R.styleable.TextShape_solidColor, Color.parseColor("#00000000"));//内部填充颜色
-        int shape = typedArray.getInt(R.styleable.TextShape_shape,0);  //0：方形  1：圆形   默认是0    如果需要设置成圆形空间的宽高必须相等  否则为椭圆
+        String shape = typedArray.getString(R.styleable.TextShape_shape);  //0：方形  1：圆形   默认是0    如果需要设置成圆形空间的宽高必须相等  否则为椭圆
         GradientDrawable gd = new GradientDrawable();//创建drawable
-        gd.setShape(shape);
+        if(!TextUtils.isEmpty(shape)&&shape.equals("oval")){
+            gd.setShape(GradientDrawable.OVAL);
+
+        }else{
+
+            gd.setShape(GradientDrawable.RECTANGLE);
+        }
+
         gd.setColor(solidColor);
         gd.setCornerRadius(roundRadius);
         gd.setStroke(strokeWidth, strokeColor);
